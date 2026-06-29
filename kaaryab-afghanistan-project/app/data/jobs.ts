5f85e7ffea8a98155abba996da082f2a481d5be6
somaiamosadeq1212 <mosadiqsomaia@gmail.com>
2026-06-28 22:43:18 -0700

    add jobs data page

---
+++ kaaryab-afghanistan-project/app/data/jobs.ts
@@ -0,0 +1,31 @@
+import { title } from "process";
+
+export const jobs = [
+    {
+        id: 1,
+        title: "Frontend Developer",
+        company: "TechVision",
+        location: "Kabul",
+        type: "Full Time",
+        salary: "$800 - $1200",
+        category: "IT",
+    },
+    {
+        id: 2,
+        title: "Graphic Designer",
+        company: "Creative Studio",
+        location: "Herat",
+        type: "Part Time",
+        salary: "$400 - $700",
+        category: "Design",
+    },
+    {
+        id: 3,
+        title: "English Teacher",
+        company: "Bright Future School",
+        loaction: "Mazar",
+        type: "Full Time",
+        sarary: "$500 - $900",
+        category: "Education",
+    }
+]
